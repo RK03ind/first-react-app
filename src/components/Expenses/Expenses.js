814638420af73bcbbca9d246f629df2d8d9ee0a5
RK03ind <gamitech95@gmail.com>
2021-12-18 19:56:32 +0530

    filter commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,23 +5,22 @@ import ExpenseFilter from "./ExpenseFilter";
 const Expenses = (props) => {
   const [year, setYear] = useState(2021);
 
+  const filteredArray = props.expenses.filter((expenses) => {
+    if (year == expenses.date.getFullYear()) return expenses;
+    return false;
+  });
+
   return (
     <>
       <ExpenseFilter value={year} postYear={setYear} />
-      {props.expenses
-        .filter((expenses) => {
-          if (year == expenses.date.getFullYear()) {
-            return expenses;
-          }
-        })
-        .map((expenses) => (
-          <ExpenseItem
-            key={expenses.id}
-            title={expenses.title}
-            amount={expenses.amount}
-            date={expenses.date}
-          />
-        ))}
+      {filteredArray.map((expenses) => (
+        <ExpenseItem
+          key={expenses.id}
+          title={expenses.title}
+          amount={expenses.amount}
+          date={expenses.date}
+        />
+      ))}
     </>
   );
 };
